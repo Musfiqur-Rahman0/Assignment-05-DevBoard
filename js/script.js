3a66354c0bfb73087ace099d29070f51540a5820
Musfiqur Rahman <musfiqurrhaman6@gmail.com>
2025-02-28 23:32:41 +0600

    added completed button event where the taskCount will decrease every time a completed button clicked and the completed task will increase also it will send a successfull massage through an aleart and however it will also add a massage with the task title in the activity log with the current time. Again i have also includes the functionality to remove all the history by clicking the clear button.

---
+++ js/script.js
@@ -0,0 +1,50 @@
+for (let completedBtn of allCompletedBtn) {
+  completedBtn.addEventListener("click", function (e) {
+    if (e.target === completedBtn) {
+      completedTaskCount += 1;
+      taskCount--;
+      completedTask.innerText = completedTaskCount;
+      cartCount.innerText = String(taskCount).padStart(2, "0");
+      completedBtn.setAttribute("disabled", true);
+
+      const task =
+        completedBtn.parentNode.parentNode.childNodes[1].childNodes[3]
+          .innerText;
+      //   console.log(task);
+      //       activityMassagesContainer.innerHTML = `
+      //          <div class="bg-gray-100 p-4 rounded-lg">
+      //
+      // </div>
+      //       `;
+      const massage = document.createElement("div");
+      massage.className = "bg-gray-100 p-4 rounded-lg";
+      massage.innerHTML = `
+        <p class="text-lg">You have Complete ${task} at ${getCurrTime()}</p>
+        `;
+      activityMassagesContainer.appendChild(massage);
+      alert("Succesfully completed the task");
+      if (taskCount === 0) {
+        alert("Congrates you have sucesfully completed all the task.");
+      }
+    }
+  });
+}
+
+function getCurrTime() {
+  const today = new Date();
+  let am_pm = "AM";
+  let hour = today.getHours();
+  const minutes = today.getMinutes();
+  const sec = today.getSeconds();
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  const currentTime = `${hour}:${minutes}:${sec} ${am_pm}`;
+  return currentTime;
+}
+
+clearBtn.addEventListener("click", function () {
+  activityMassagesContainer.innerHTML = "";
+  console.log("clicked");
+});
---
+++ js/utilities.js
@@ -0,0 +1,14 @@
+const allCompletedBtn = document.querySelectorAll("#completedBtn");
+const completedTask = document.getElementById("completedTaskCount");
+const cartCount = document.getElementById("cart-count");
+const activityMassagesContainer = document.getElementById("activity-container");
+const clearBtn = document.getElementById("clear-task");
+
+let completedTaskCount = parseInt(completedTask.innerText);
+let taskCount = parseInt(cartCount.innerText);
+// console.log(taskCount);
+{
+  /* <div class="bg-gray-100 p-4 rounded-lg">
+<p class="text-lg">You have Complete The Task Add Dark Mode at 12:48:15 PM</p>
+</div> */
+}
